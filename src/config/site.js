f70fdf0ed4b7aebd805b5f63819b06a3188410b5
𝑬𝑵𝑮. 𝑬𝑳𝒀𝑨𝑺 𝑨𝑳 𝑺𝑯𝑨𝑬𝑩𝑰 <em775377979@gmail.com>
2026-09-21 02:00:46 +0300

    feat(vnext): add src/config/site.js

---
+++ src/config/site.js
@@ -0,0 +1,21 @@
+export const site = Object.freeze({
+  brand: Object.freeze({
+    ar: "وصل تك",
+    en: "Wasl Tech",
+    slogan: "نبني حضورك الرقمي باحتراف",
+    logo: "/assets/brand/wasl-tech-horizontal.svg",
+    logoWhite: "/assets/brand/wasl-tech-horizontal-white.svg"
+  }),
+  locale: Object.freeze({
+    lang: "ar",
+    dir: "rtl"
+  }),
+  market: "اليمن + الخليج",
+  contact: Object.freeze({
+    phoneDisplay: "+967 775 377 979",
+    phoneUri: "+967775377979",
+    whatsapp: "https://wa.me/967775377979",
+    email: "wasltechp@gmail.com",
+    domain: "www.wasl-tech.com"
+  })
+});
