0c940b190602f49a82789fcc4d071877095fd7fc
𝑬𝑵𝑮. 𝑬𝑳𝒀𝑨𝑺 𝑨𝑳 𝑺𝑯𝑨𝑬𝑩𝑰 <em775377979@gmail.com>
2026-09-21 04:06:32 +0300

    refactor(vnext): source site identity from brand config

--- src/config/site.js
+++ src/config/site.js
@@ -1,11 +1,7 @@
+import { brand } from "./brand.js";
+
 export const site = Object.freeze({
-  brand: Object.freeze({
-    ar: "وصل تك",
-    en: "Wasl Tech",
-    slogan: "نبني حضورك الرقمي باحتراف",
-    logo: "/assets/brand/wasl-tech-horizontal.svg",
-    logoWhite: "/assets/brand/wasl-tech-horizontal-white.svg"
-  }),
+  brand,
   locale: Object.freeze({
     lang: "ar",
     dir: "rtl"
